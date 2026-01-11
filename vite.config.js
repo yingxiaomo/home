362647c61bddac1ec9b0d52bcd7ef68451674cab
YingXiaoMo <yingxiaomoya@gmail.com>
2026-01-11 18:05:15 +0800

    禁用压缩插件以排查构建卡死问题

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteCompression from 'vite-plugin-compression'
+// import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
   plugins: [
     vue(),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
+    // viteCompression({
+    //   verbose: true,
+    //   disable: false,
+    //   threshold: 10240,
+    //   algorithm: 'gzip',
+    //   ext: '.gz',
+    // }),
   ],
   resolve: {
     alias: {
